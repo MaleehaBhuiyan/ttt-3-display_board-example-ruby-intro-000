0becf9610d6b99e9dd6ce503659b35f8ca9cfb42
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-04-05 17:15:31 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-def display_board
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board(board)
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
@@ -7,4 +9,4 @@ def display_board
   puts "   |   |   "
 end
 
-display_board 
+display_board(board)
